ab49bdeb7e98dfa789bab14c06d038a5804a9b8d
Eunice Kim <ekimed@gmail.com>
2014-06-21 16:12:03 -0700

    split directives into files

--- public/scripts/temp/directives.js
+++ public/scripts/temp/directives.js
@@ -1,35 +1,5 @@
 'use strict';
 angular.module('pillboxApp.directive', []);
-//Generate universal unique id for dragged elements
-angular.module('pillboxApp.directive').factory('uuid', function () {
-  var svc = {
-      new: function () {
-        function _p8(s) {
-          var p = (Math.random().toString(16) + '000000000').substr(2, 8);
-          return s ? '-' + p.substr(0, 4) + '-' + p.substr(4, 4) : p;
-        }
-        return _p8() + _p8(true) + _p8(true) + _p8();
-      },
-      empty: function () {
-        return '00000000-0000-0000-0000-000000000000';
-      }
-    };
-  return svc;
-});
-angular.module('pillboxApp.directive').directive('fileModel', [
-  '$parse',
-  function ($parse) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        element.bind('change', function () {
-          $parse(attrs.fileModel).assign(scope, element[0].files[0]);
-          scope.$apply();
-        });
-      }
-    };
-  }
-]);
 angular.module('pillboxApp.directive').directive('isDraggable', [
   '$rootScope',
   function ($rootScope) {
@@ -120,4 +90,33 @@ angular.module('pillboxApp.directive').directive('dropTarget', [
       }
     };
   }
-]);
+]);// File upload directive
+angular.module('pillboxApp.directive').directive('fileModel', [
+  '$parse',
+  function ($parse) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.bind('change', function () {
+          $parse(attrs.fileModel).assign(scope, element[0].files[0]);
+          scope.$apply();
+        });
+      }
+    };
+  }
+]);// Generate universal unique id for dragged elements
+angular.module('pillboxApp.directive').factory('uuid', function () {
+  var svc = {
+      new: function () {
+        function _p8(s) {
+          var p = (Math.random().toString(16) + '000000000').substr(2, 8);
+          return s ? '-' + p.substr(0, 4) + '-' + p.substr(4, 4) : p;
+        }
+        return _p8() + _p8(true) + _p8(true) + _p8();
+      },
+      empty: function () {
+        return '00000000-0000-0000-0000-000000000000';
+      }
+    };
+  return svc;
+});
