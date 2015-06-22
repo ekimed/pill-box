9b9b188a41a61dc68610eb779f7c5b1fb344e423
Eunice <eunice.kim@11main.com>
2015-06-21 20:45:37 -0700

    Added new route for rxterms

--- app.js
+++ app.js
@@ -13,17 +13,20 @@ app.use(bodyParser());
 app.use(multipart());
 
 // Connect to database
-mongoose.connect(process.env.MONGOHQ_URL||'mongodb://localhost/RxTerms');
+//mongoose.connect(process.env.MONGOHQ_URL||'mongodb://localhost/RxTerms');
+mongoose.connect('mongodb://localhost/medications');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback (){
 	console.log('mongodb was successfully connected');
-})
+});
 app.get('/', routes.layout);
 app.get('/partials/:name', routes.partials);
 app.post('/fileupload', routes.upload);
+app.get('/rxterms', routes.rxterms);
 app.get('*', routes.layout);
+
 
 
 
