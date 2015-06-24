fc536a541d630334fea6ec08d85e64ac12ecdfdd
Eunice <eunice.kim@11main.com>
2015-06-23 22:22:17 -0700

    Updated elasticsearch query to partial match multiple fields

---
+++ src/public/scripts/app.js
@@ -0,0 +1,29 @@
+angular.module('pillboxApp', ['angularFileUpload', 'pillboxApp.service', 'pillboxApp.directive','ngResource', 'ngRoute', 'elasticsearch'])
+	.config(function($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode(true);
+		$routeProvider
+			.when('/test', {
+				templateUrl: 'partials/test.jade'
+			})
+			.when('/welcome', {
+				templateUrl: 'partials/welcome.jade',
+				controller: 'FileController'
+			})
+			.when('/index', {
+				templateUrl: 'partials/main.jade'
+			})
+			.when('/list', {
+				templateUrl: 'partials/list.jade',
+				controller: 'MedListCtrl'
+			})
+			.when('/schedule', {
+				templateUrl: 'partials/schedule.jade'
+			})
+            .when('/search', {
+                templateUrl: 'partials/searchMeds.jade',
+                controller: 'SearchMedListCtrl'
+            })
+			.otherwise({
+        		redirectTo: '/welcome'
+      		});
+	});
