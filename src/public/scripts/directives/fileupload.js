fa9257e93edca0f56a8974231383b3723dbbc88f
Eunice <eunice.kim@11main.com>
2015-06-23 22:27:54 -0700

    file restructure

---
+++ src/public/scripts/directives/fileupload.js
@@ -0,0 +1,20 @@
+// File upload directive
+angular.module('pillboxApp.directive')
+	.directive('fileModel',['$parse', function($parse){
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+				element.bind('change', function() {
+					$parse(attrs.fileModel)
+						.assign(scope,element[0].files[0])
+					scope.$apply();
+				})
+			}
+		}
+	}]);
+
+
+
+
+
+
