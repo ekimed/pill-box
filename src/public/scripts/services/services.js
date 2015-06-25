ec0d4f6205b6b10db7447271bf9287b0dc1be36b
Eunice <eunice.kim@11main.com>
2015-06-24 22:59:31 -0700

    Refactor

--- src/public/scripts/services/services.js
+++ src/public/scripts/services/services.js
@@ -1,33 +1,44 @@
-angular.module('pillboxApp.service', []);
+(function() {
+    angular.module('pillboxApp.service', []);
 
-angular.module('pillboxApp.service')
-	.service('fileUpload', ['$http', function($http) {
-		this.uploadFileToUrl = function(file, uploadUrl) {
-			console.log(file)
-			var fd = new FormData();
-			fd.append('file', file);
-			$http.post(uploadUrl, fd, {
-				transformRequest: angular.identity,
-				headers: {'Content-Type': undefined}
-			})
-			.success(function(d) {
-				console.log('Success! Data was sent to server!')
-			})
-			.error(function(error) {
-				console.error('Error! Something went wrong when sending data to server!', error)
-			});
-		}
-	}]);
+    angular.module('pillboxApp.service')
+        .service('fileUpload', ['$http', function($http) {
+            this.uploadFileToUrl = function(file, uploadUrl) {
+                console.log(file)
+                var fd = new FormData();
+                fd.append('file', file);
+                $http.post(uploadUrl, fd, {
+                    transformRequest: angular.identity,
+                    headers: {'Content-Type': undefined}
+                })
+                    .success(function(d) {
+                        console.log('Success! Data was sent to server!')
+                    })
+                    .error(function(error) {
+                        console.error('Error! Something went wrong when sending data to server!', error)
+                    });
+            }
+        }]);
 
-// esFactory() creates a configured client instance. Turn that instance
-// into a service so that it can be required by other parts of the application
-angular.module('pillboxApp.service')
-   .service('esService', ['esFactory', '$q', '$location', function (esFactory, $q, $location) {
-       return esFactory({
-           host: 'localhost:9200',
-           apiVersion: '1.6',
-           log:'trace'
-       });
-   }]);
+    // esFactory() creates a configured client instance. Turn that instance
+    // into a service so that it can be required by other parts of the application
+    angular.module('pillboxApp.service')
+        .service('esService', ['esFactory', function (esFactory) {
+            return esFactory({
+                host: 'localhost:9200',
+                apiVersion: '1.6',
+                log:'trace'
+            });
+        }]);
+})();
+
+
+
+
+
+
+
+
+
 
 
