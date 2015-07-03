9a8801de2fd2d0afc4cd0e562f05bbe4fec0ca02
Eunice <eunice.kim@11main.com>
2015-07-03 02:10:00 -0700

    added flatten fn and formatted DDI data sent from API

--- src/public/scripts/services/ddi_data_service.js
+++ src/public/scripts/services/ddi_data_service.js
@@ -9,24 +9,27 @@
         .service('DrugInteraction', [function () {
             var interactions = [];
             var drugPair = [];
-            var Type = function (description, drugs) {
+            var _drugList = [];
+            var Schema = function (description, drug1, drug2) {
                 this.description = description;
-                this.drugs = drugs;
+                this.drug_a = drug1;
+                this.drug_b = drug2;
             };
-            var Drug = function (name, url) {
-                this.name = name;
-                this.url = url;
-            };
+
+            function isObject (o) {
+                if (o instanceof Object && !(o instanceof Array)) {
+                    return true;
+                }
+
+                return false;
+            }
+
+            function isFunction (fn) {
+                return !!(fn && fn.constructor && fn.call && fn.apply);
+            }
 
             function traverse (obj, fn) {
                 var val, check, val2;
-                var isObject = function (o) {
-                    if (o instanceof Object && !(o instanceof Array)) {
-                        return true;
-                    }
-
-                    return false;
-                };
 
                 for (var key in obj) {
                     val = obj[key];
@@ -57,8 +60,28 @@
                 return results;
             }
 
+            function _flatten (data) {
+                var arr = [];
+                function fn (array) {
+                    for (var i = 0; i < array.length; i++) {
+                        if (array[i].constructor !== Array) {
+                            arr.push(array[i]);
+                        } else if (array[i].constructor === Array) {
+                            fn(array[i]);
+                        }
+                    }
+                }
+
+                fn(data);
+
+                return arr;
+            }
+
+            // TODO filter array only unique objects
+
             return {
                 format: function (data) {
+                    var res = [];
                     var terms = [
                         'fullInteractionType',
                         'interactionPair',
@@ -67,8 +90,31 @@
                         'sourceConceptItem',
                         'name'
                     ];
+                    var tmp;
 
-                    var fullIntType = this.find(data, terms[0]); console.log(fullIntType);
+                    interactions = this.find(data, terms[0])[0]; // should return an array
+
+                    interactions.forEach(function (type) {
+                        tmp = getProp(type, terms[1]);
+                        drugPair = drugPair.concat(_flatten(tmp));
+
+                    });
+
+                    drugPair.forEach(function (drug) {
+                        var a, b;
+                        _drugList = drug['interactionConcept'];
+                        _drugList.forEach(function (concept) {
+                            if (a) {
+                                b = concept['sourceConceptItem'].name;
+                            } else {
+                                a = concept['sourceConceptItem'].name;
+                            }
+                        });
+                        var _drug = new Schema(drug.description, a, b);
+                        res.push(_drug);
+                    });
+
+                    return res;
                 },
                 find: function (data, term) {
                     var res = getProp(data, term);
