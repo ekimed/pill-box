9a0d1852e1646ce9fa01029517da55acf128e8e8
Eunice <eunice.kim@11main.com>
2015-07-06 10:45:20 -0700

    carousel dropdown highlighting added

--- src/public/scripts/temp/templates.js
+++ src/public/scripts/temp/templates.js
@@ -2,5 +2,5 @@ angular.module('templates-main', ['server/views/modal/templates/ddi_template.jad
 
 angular.module("server/views/modal/templates/ddi_template.jade", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("server/views/modal/templates/ddi_template.jade",
-    "<div ng-model=\"ddiData\" class=\"ngdialog-message\"><h2>{{ddiLen}} Drug Interaction Pairs Found</h2><div ng-repeat=\"d in ddiData\"><div>{{d.drug_a}} + {{d.drug_b}} <i class=\"fa fa-long-arrow-right ddi-arrow\"></i> <i class=\"fa fa-exclamation-triangle ddi-warn\"></i> {{d.description}}</div></div></div>");
+    "<div ng-model=\"ddiData\" class=\"ngdialog-message\"><h2>{{ddiLen}} Drug Interaction Pairs Found</h2><div ng-repeat=\"d in ddiData\" class=\"ddi-item\"><div>{{d.drug_a}} + {{d.drug_b}} <i class=\"fa fa-long-arrow-right ddi-arrow\"></i> <i class=\"fa fa-exclamation-triangle ddi-warn\"></i> {{d.description}}</div></div></div>");
 }]);
